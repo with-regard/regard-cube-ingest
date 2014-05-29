b34e54a7372876a7cb9f8f2dec5002cef5d3ed84
Kevin Boyle <kevin.boyle@outlook.com>
2014-05-29 10:31:28 +0100

    Correct sln for build

--- build.bat
+++ build.bat
@@ -7,5 +7,5 @@ echo Looking for nuget sources at: %NuGetSources%
 
 .nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages -source "%NuGetSources%" 
 
-.nuget\NuGet.exe restore Regard.Consumer.sln -source "%NuGetSources%" 
+.nuget\NuGet.exe restore regard-cube-collector.sln -source "%NuGetSources%" 
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\default.ps1 %*; if ($LastExitCode -ne 0) {write-host "ERROR: $LastExitCode" -fore RED; exit $lastexitcode} }"
